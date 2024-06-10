86e471192a778314ed7a8d80178b1e849c36adf4
lyDiyamong <diyamong26@gmail.com>
2024-06-10 15:24:48 +0700

    add possibility

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,7 @@ import ai from '../../assets/gpt3/ai.png'
 
 function Header() {
   return (
-    <div className='gpt3__header section_padding'>
+    <div className='gpt3__header section_padding' id='#home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let's Built Something amazing with GPT-3 OpenAI</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
